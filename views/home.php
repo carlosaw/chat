6be13834efb905774818aece589ab91578c5a638
Carlos Alberto <carlosfreevg@gmail.com>
2021-12-01 16:57:21 -0400

    Área de digitar mensagem

--- views/home.php
+++ views/home.php
@@ -7,7 +7,46 @@
 	</nav>
 
 	<section>
-		<div class="messages"></div>					
+		<div class="messages">
+
+			<div class="message">
+				<div class="m_info">
+					<span class="m_sender">Carlos Alberto</span>
+					<span class="m_date">10:00</span>
+				</div>
+				<div class="m_body">
+					Alguma mensagem de exemplo.
+				</div>
+			</div>
+			<div class="message">
+				<div class="m_info">
+					<span class="m_sender">Carlos Alberto</span>
+					<span class="m_date">10:00</span>
+				</div>
+				<div class="m_body">
+					Alguma mensagem de exemplo.
+				</div>
+			</div>
+			<div class="message">
+				<div class="m_info">
+					<span class="m_sender">Carlos Alberto</span>
+					<span class="m_date">10:00</span>
+				</div>
+				<div class="m_body">
+					Alguma mensagem de exemplo.
+				</div>
+			</div>
+			<div class="message">
+				<div class="m_info">
+					<span class="m_sender">Carlos Alberto</span>
+					<span class="m_date">10:00</span>
+				</div>
+				<div class="m_body">
+					Alguma mensagem de exemplo.
+				</div>
+			</div>
+
+		</div>				
 	</section>
 
 	<footer>
